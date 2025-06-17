e31a8c3fd13cdcd4f86cabb8fde9e83933f25c28
m67pg <m_owaki@nifty.com>
2025-06-17 14:44:01 +0900

    【改善】ラベルを表示するように修正

--- templates/form-template.php
+++ templates/form-template.php
@@ -1,19 +1,19 @@
 <div id="form-message" style="color:red;"></div>
 <form id="crowd_sourcing-form">
     <p>
-        <Label for="name" value="名前" />
+        <label for="name" value="名前" />
 
         <input type="text" name="name" id="name" style="width:300px;">
         <br><span id="error-name" style="color:red;"></span>
     </p>
     <p>
-        <Label for="sort_order" value="並び順" />
+        <label for="sort_order" value="並び順" />
 
         <input type="text" name="sort_order" id="sort_order" style="width:300px;">
         <br><span id="error-sort_order" style="color:red;"></span>
     </p>
     <p>
-        <Label for="display" value="表示 / 非表示" />
+        <label for="display" value="表示 / 非表示" />
 
         <input type="radio" name="display" value="1">表示
         <input type="radio" name="display" value="0">非表示
